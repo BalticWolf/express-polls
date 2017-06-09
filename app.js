80a7068a16b2b37b63bee32172ca2f4d8c848511
Timothee AUPETIT <timothee.aupetit@gmail.com>
2017-06-09 11:08:20 +0200

    created routes.js and adapted app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const settings = require('./settings');
 const express = require('express');
 const bodyParser = require('body-parser'); // ceci est un middleware qui formate la requete
 const usersRoutes = require('./routes/users');
+const pollsRoutes = require('./routes/polls');
 const authMiddleware = require ('./authMiddleware');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(authMiddleware);
 
 app.use('/users', usersRoutes);
+app.use('/polls', pollsRoutes);
 
 app.get('/', (req, res) =>{
     res.send('It works!');
---
+++ routes/polls.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id', (req, res) =>{
+    const id = parseInt(req.params.id, 10);
+    const poll = users.find(u => u.id === id);
+
+    if(!poll) return res.status(404).send('User not found');
+    res.send(poll);
+});
