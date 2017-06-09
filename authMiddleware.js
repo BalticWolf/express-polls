e09d04524b99069c382781e8ae9ac0838ee38294
Timothee AUPETIT <timothee.aupetit@gmail.com>
2017-06-09 11:35:18 +0200

    minor corrections on module imports

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,3 +1,6 @@
+const base64 = require('js-base64').Base64;
+const settings = require('./settings');
+
 module.exports = (req, res, next) => {
     try {
         const authorization = req.headers.authorization;
@@ -7,10 +10,17 @@ module.exports = (req, res, next) => {
         const match = authorization.match(/^Basic (.+)$/);
 
         if (!match) throw new Error ('Invalid header authorization');
-        //= settings.admin.username + ':' + settings.admin.password;
 
+        const credentials = base64.decode(match[1]).split(':');
+
+        if (credentials.length !== 2
+            || credentials[0] !== settings.admin.username
+            || credentials[1] !== settings.admin.password) {
+            throw new Error('Incorrect username or password');
+        }
         next();
     } catch(e) {
+        res.setHeader('WWW-Authentificate', 'Basic realm="Restricted access"');
         res.status(401).send(e.message);
     }
 };
