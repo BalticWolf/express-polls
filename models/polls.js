06e51632297b601829d2957f739ad5940c20f650
Timothee AUPETIT <timothee.aupetit@gmail.com>
2017-06-09 11:34:32 +0200

    added models

---
+++ models/polls.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        question: '...?',
+        answers: ['yes', 'no'],
+        votes: []
+    },
+    {
+        id: 2,
+        question: '...?',
+        answers: ['yes', 'no', 'maybe', "i don't know", 'can you repeat the question?'],
+        votes: []
+    }
+];
---
+++ models/users.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 1,
+        username: 'Timo',
+        sex: 'M'
+    },
+    {
+        id: 2,
+        username: 'Julie',
+        sex: 'F'
+    }
+];
