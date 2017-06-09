614dcee07798d1670a30d2980ff565786eb57c7a
Timothee AUPETIT <timothee.aupetit@gmail.com>
2017-06-09 12:09:29 +0200

    implemented create, read and delete

--- models/polls.js
+++ models/polls.js
@@ -1,13 +1,13 @@
 module.exports = [
     {
         id: 1,
-        question: '...?',
+        question: 'Do you like binary code?',
         answers: ['yes', 'no'],
         votes: []
     },
     {
         id: 2,
-        question: '...?',
+        question: 'Are you the boss of me?',
         answers: ['yes', 'no', 'maybe', "i don't know", 'can you repeat the question?'],
         votes: []
     }
--- routes/polls.js
+++ routes/polls.js
@@ -2,12 +2,61 @@ const express = require('express');
 const polls = require('../models/polls');
 const router = express.Router();
 
-router.get('/:id', (req, res) =>{
-    const id = parseInt(req.params.id, 10);
-    const poll = users.find(u => u.id === id);
+// Récupération de la liste des sondages
+router.get('/', (req, res) => {
+    res.send(polls);
+});
 
-    if(!poll) return res.status(404).send('User not found');
+// Récupération d'un sondage selon son id
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const poll = polls.find(u => u.id === id);
+
+    if(!poll) return res.status(404).send('Poll not found');
     res.send(poll);
+});
+
+// Ajout d'un sondage
+router.post('/', (req, res) => {
+    const { question, answers } = req.body;
+
+    if (!question) return res.status(400).send('Missing parameter question');
+    if (!answers) return res.status(400).send('Missing parameter answers');
+
+    const maxId = Math.max(...polls.map(p => p.id));
+
+    const poll = {
+         id: maxId + 1,
+         question: question,
+         answers: answers,
+         votes: []
+    };
+
+    polls.push(poll);
+    res.status(201).send(poll);
+});
+
+// Suppression d'un sondage par son id
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const poll = polls.find(u => u.id === id);
+    if(!poll) return res.sendStatus(404);
+
+    const index = polls.findIndex(u => u.id === id);
+    polls.splice(index, 1); // supprime un seul élément à partir de index
+
+    res.status(204);
+});
+
+// Voter à un sondage (identifié par son id)
+router.post('/:id/vote', (req, res) => {
+    const answer = req.body.answer;
+    const id = parseInt(req.params.id, 10);
+    const poll = polls.find(u => u.id === id);
+    if(!poll) return res.sendStatus(404);
+
+    if (!answer) return res.status(400).send('Missing parameter answer');
+    
 });
 
 module.exports = router;
